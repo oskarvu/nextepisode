2ecf2cff5d8fb8a1d85e4e09d2f3ccedb65020d7
oskarvu <oskar@enso.finance>
2023-05-14 11:35:38 +0200

    Add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { inject } from '@vercel/analytics'
 import React from 'react'
-import 'tailwindcss/dist/base.min.css'
 import { RecoilRoot } from 'recoil'
+import 'tailwindcss/dist/base.min.css'
 
-import { TopBar } from './components/TopBar/TopBar'
-import { MovieCollection } from './components/MovieCollection/MovieCollection'
 import { Footer } from './components/Footer/Footer'
 import { MainContainer } from './components/MainContainer/MainContainer'
+import { MovieCollection } from './components/MovieCollection/MovieCollection'
+import { TopBar } from './components/TopBar/TopBar'
+
+inject()
 
 export default function App() {
   return (
